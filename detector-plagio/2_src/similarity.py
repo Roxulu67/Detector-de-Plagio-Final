62deb6ba0fa77269243394a5c961ca150d9b09d1
Roxulu67 <rodrigogt03@gmail.com>
2025-04-08 16:55:19 -0600

    Delete detector-plagio/2_src/similarity.py

--- detector-plagio/2_src/similarity.py
+++
@@ -1,5 +0,0 @@
-def calculate_jaccard_similarity(set_a, set_b):
-    """Calcula la similitud de Jaccard entre dos conjuntos."""
-    intersection = len(set_a.intersection(set_b))
-    union = len(set_a.union(set_b))
-    return intersection / union if union != 0 else 0
